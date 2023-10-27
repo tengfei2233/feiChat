d9cf7c81b501a07ca93b1b1214971b7a7e71098e
tengfei2233 <2792498102@qq.com>
2023-10-27 17:24:39 +0800

    杂项

--- pages/friend/friend.js
+++ pages/friend/friend.js
@@ -1,32 +1,110 @@
-// pages/friend/friend.js
+const res = {
+    result: [
+        [{
+            cidx: [0, 15],
+            fullname: "北京市",
+            id: "110000",
+            location: {
+                lat: 39.90469,
+                lng: 116.40717
+            },
+            name: "北京",
+            pinyin: ["bei", "jing"]
+        }, {
+            cidx: [16, 31],
+            fullname: "天津市",
+            id: "120000",
+            location: {
+                lat: 39.0851,
+                lng: 117.19937
+            },
+            name: "天津",
+            pinyin: ["tian", "jin"]
+        }, {
+            cidx: [32, 42],
+            fullname: "河北省",
+            id: "130000",
+            location: {
+                lat: 38.03599,
+                lng: 114.46979
+            },
+            name: "河北",
+            pinyin: ["he", "bei"],
+        }, {
+            cidx: [43, 53],
+            fullname: "山西省",
+            id: "140000",
+            location: {
+                lat: 37.87343,
+                lng: 112.56272
+            },
+            name: "山西",
+            pinyin: ["shan", "xi"],
+        }]
+    ]
+}
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        searchVal: ''
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad(options) {
-
+        this.getCitys()
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
+    onChoose(e) {
+        console.log('onChoose', e)
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
     onShow() {
         this.getTabBar().init();
     },
+
+    search(e) {
+
+    },
+    add(e) {
+
+    },
+
+    getCitys() {
+        const _this = this
+        const cities = res.result[0]
+        // 按拼音排序
+        cities.sort((c1, c2) => {
+            let pinyin1 = c1.pinyin.join('')
+            let pinyin2 = c2.pinyin.join('')
+            return pinyin1.localeCompare(pinyin2)
+        })
+        // 添加首字母
+        const map = new Map()
+        for (const city of cities) {
+            const alpha = city.pinyin[0].charAt(0).toUpperCase()
+            if (!map.has(alpha)) map.set(alpha, [])
+            map.get(alpha).push({
+                name: city.fullname
+            })
+        }
+
+        const keys = []
+        for (const key of map.keys()) {
+            keys.push(key)
+        }
+        keys.sort()
+
+        const list = []
+        for (const key of keys) {
+            list.push({
+                alpha: key,
+                subItems: map.get(key)
+            })
+        }
+
+        _this.setData({
+            list
+        })
+    }
 
 })
